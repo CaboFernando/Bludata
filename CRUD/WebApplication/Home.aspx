1a7223495e200144761d89558dfd22798b957fbd
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2019-05-03 14:21:36 -0300

    Create views

---
+++ CRUD/WebApplication/Home.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="WebApplication.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Home</title>
+    <link type="text/css" rel="stylesheet" href="Content/bootstrap.css" />
+</head>
+<script src="Scripts/jquery-3.0.0.min.js"></script>
+<script src="Scripts/bootstrap.min.js"></script>
+
+<body>
+    <form id="form1" runat="server">
+        <div class="jumbotron">
+            <h1>Avaliação de Conhecimentos – Programador C#</h1>
+
+            Selecione a operação desejada:
+            <asp:DropDownList ID="ddlMenu" runat="server">
+                <asp:ListItem Value="0" text="- Escolha uma opção -" />
+                <asp:ListItem Value="1" text="Cadastrar Empresa" />
+                <asp:ListItem Value="2" text="Consultar Empresa" />
+            </asp:DropDownList>
+            <asp:Button ID="btnMenu" runat="server" text="Acessar" CssClass="btn btn-primary btn-lg"/>
+            <p>
+                <asp:Label ID="lblMensagem" runat="server"/>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CRUD/WebApplication/Home.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication {
+    
+    
+    public partial class Home {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle ddlMenu.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMenu;
+        
+        /// <summary>
+        /// Controle btnMenu.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMenu;
+        
+        /// <summary>
+        /// Controle lblMensagem.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensagem;
+    }
+}
